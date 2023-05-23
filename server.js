9a8b5f229b93156f9f7a16e19e68212035957b54
myhnk <myhnk@devcamp.edu.vn>
2023-05-23 20:52:56 +0700

    [EcomGAS-001] Build project and organize the folder structure

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('./src/app');
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+    console.log(`Ecommerce server start on port: ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    server.close(() => console.log(`Exit Server Express`));
+    // notyfi.send('....');
+})
+
---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//init middledwares
+
+//init db
+
+//init routers
+
+//handling errors
+
+module.exports = app;
