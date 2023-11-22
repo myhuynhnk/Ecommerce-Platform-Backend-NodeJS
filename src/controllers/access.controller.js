5ff674ef58f8dc517a4b3194041859fb15a077fc
mhuynh9 <myngockhanh.huynh@dxc.com>
2023-11-22 14:32:14 +0700

    [EcomGAS-008] Handle API response use class - build success.response.js classes response

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const AccessService = require('../services/access.service');
+const { OK, CREATED } = require('../core/success.response');
 
 class AccessController {
 
@@ -11,7 +12,15 @@ class AccessController {
         // } catch (error) {
         //     next(error);
         // }
-        return res.status(201).json( await AccessService.signUp(req.body));
+
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10
+            }
+        }).send(res);
+        // return res.status(201).json( await AccessService.signUp(req.body));
     }
 }
 
